2e3c1f797eb3edf3283d81d7f4436490b82d1144
unknown <falook555@gmail.com>
2020-01-03 11:19:26 +0700

    ss

--- routes.js
+++ routes.js
@@ -5,9 +5,6 @@ const passportService = require('./service/passport')
 const requireSignin = passport.authenticate('local', { session: false })
 const requireAuth = passport.authenticate('jwt', { session: false })
 const users = require('./controllers/Users')
-const Patient = require('./controllers/Patient')
-const Getpdf = require('./controllers/GetPdf/getPdf')
-const AddLog = require('./controllers/Log/AddLog')
 const EletIndex = require('./controllers/elect/index')
 
 
@@ -23,19 +20,6 @@ module.exports = function (app) {
     })
     app.post('/signin', requireSignin, users.signin)
 
-   
-    app.get('/find-hn/:hn',Patient.findPatient)
-    // app.get('/users',requireAuth, users.findAll)
-    // app.get('/chkuser/:id',requireAuth,users.findUser)
-    // app.get('/profile/:id',requireAuth,users.findProfile)
-    // app.get('/profile-pay/:id',requireAuth,users.findProfilePay)
-    // app.get('/profile-history/:id',requireAuth,users.findHistory)
-    // app.get('/user-staff/:id',requireAuth,users.findUserStaff)
-    // app.get('/post-count/:id',requireAuth,users.findPostCount)
-    // app.get('/auction-count/:id',requireAuth,users.findAuctionCount)
-
-    app.get('/get-pdf/:hn',Getpdf.getPdf)
-    app.post('/log-add',AddLog.create)
 
     app.get('/get-indexeletri',EletIndex.findeletri)
     app.get('/get-main/:id',EletIndex.showmonth)
